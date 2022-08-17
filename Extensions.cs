8c389e37749875ac6ad00fa88d15310c26257f21
Pixelmania <84938061+Malishade@users.noreply.github.com>
2022-08-18 01:20:36 +0200

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,88 @@
+﻿using AOSharp.Clientless;
+using AOSharp.Common.GameData;
+using SmokeLounge.AOtomation.Messaging.GameData;
+using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace MalisBuffBots
+{
+    public class Extensions
+    {
+        public static string PluginDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static void RemoveBuff(int nanoId)
+        {
+            Client.Send(new CharacterActionMessage
+            {
+                Action = CharacterActionType.RemoveFriendlyNano,
+                Parameter1 = 53019,
+                Parameter2 = nanoId,
+            });
+        }
+
+        public static int FindBestNcuNano(int playerLevel)
+        {
+            if (25 <= playerLevel && playerLevel < 50) { return Nanos.RetoolNCU; }
+            else if (50 <= playerLevel && playerLevel < 75) { return Nanos.JuryRiggedNCUAnalyzer; }
+            else if (75 <= playerLevel && playerLevel < 125) { return Nanos.DeckRecoder; }
+            else if (125 <= playerLevel && playerLevel < 135) { return Nanos.RecompilingMemoryAnalyzer; }
+            else if (135 <= playerLevel && playerLevel < 165) { return Nanos.QuarkStorNCUCOre; }
+            else if (165 <= playerLevel && playerLevel < 185) { return Nanos.ActiveViralCompressor; }
+            else return Nanos.SentinentViralRecoder;
+        }
+
+        public static bool IsNcuNano(int nanoId)
+        {
+            if (nanoId == Nanos.RetoolNCU ||
+                nanoId == Nanos.JuryRiggedNCUAnalyzer ||
+                nanoId == Nanos.DeckRecoder ||
+                nanoId == Nanos.RecompilingMemoryAnalyzer ||
+                nanoId == Nanos.QuarkStorNCUCOre ||
+                nanoId == Nanos.ActiveViralCompressor ||
+                nanoId == Nanos.SentinentViralRecoder)
+                return true;
+            return false;
+        }
+    }
+
+    public class BuffEntry
+    {
+        public SimpleChar Character;
+        public NanoEntry NanoEntry;
+    }
+
+    public class NanoEntry
+    {
+        public string Name;
+        public int Id;
+        public CastType Type;
+        public List<string> Tags;
+    }
+
+    public enum CastType
+    {
+        Single,
+        Team
+    }
+
+    public class Nanos
+    {
+        public const int BeaconWarp = 154914;
+        public const int TeamBeaconWarp = 154913;
+        public const int RetoolNCU = 163079;
+        public const int JuryRiggedNCUAnalyzer = 163081;
+        public const int DeckRecoder = 163083;
+        public const int RecompilingMemoryAnalyzer = 163085;
+        public const int QuarkStorNCUCOre = 163087;
+        public const int ActiveViralCompressor = 163094;
+        public const int SentinentViralRecoder = 163095;
+        public const int SuperiorOmniMedEnhancement = 95709;
+        public const int SloughingCombatField = 204422;
+        public const int GridspaceFreedomTeam = 162595;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,183 @@
+﻿using AOSharp.Clientless;
+using AOSharp.Clientless.Chat;
+using AOSharp.Clientless.Logging;
+using AOSharp.Common.GameData;
+using Newtonsoft.Json;
+using SmokeLounge.AOtomation.Messaging.GameData;
+using SmokeLounge.AOtomation.Messaging.Messages;
+using SmokeLounge.AOtomation.Messaging.Messages.N3Messages;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace MalisBuffBots
+{
+    public class Main : ClientlessPluginEntry
+    {
+        private static Dictionary<Profession, List<NanoEntry>> _nanoDb;
+        private List<BuffEntry> _buffEntries;
+        private double _waitTime;
+        private double _graceTime;
+        private BuffEntry _currentBuffEntry;
+        private bool _isInTeam = false;
+        private bool _sentTeamRequest = false;
+        public override void Init(string pluginDir)
+        {
+            Logger.Information("- Mali's Clientless Buffbots -");
+
+            Client.OnUpdate += OnUpdate;
+            Client.MessageReceived += OnMessageReceived;
+            Client.Chat.VicinityMessageReceived += (e, msg) => HandleVicinityMessage(msg);
+
+            _buffEntries = new List<BuffEntry>();
+            _nanoDb = JsonConvert.DeserializeObject<Dictionary<Profession, List<NanoEntry>>>(File.ReadAllText($@"{Extensions.PluginDir}\BuffsDb.json"));
+            _graceTime = 0.5f;
+        }
+
+        private void HandleVicinityMessage(VicinityMsg msg)
+        {
+            string[] commandParts = msg.Message.Split(' ');
+
+            switch (commandParts[0])
+            {
+                case "cast":
+                    {
+                        Logger.Information($"Received cast request from {msg.SenderName}");
+
+                        if (commandParts.Length < 2)
+                        {
+                            Logger.Error($"Received cast command without valid params.");
+                            return;
+                        }
+
+                        if (!DynelManager.Find(msg.SenderName, out PlayerChar requestor))
+                        {
+                            Logger.Error($"Unable to locate requestor.");
+                            return;
+                        }
+
+                        foreach (var reqNano in commandParts.Skip(1).Distinct())
+                        {
+                            DynelManager.Find(new Identity(IdentityType.SimpleChar, Client.LocalDynelId), out PlayerChar playerChar);
+
+                            var dictNano = _nanoDb[DynelManager.LocalPlayer.Profession].FirstOrDefault(x => x.Tags.Contains(reqNano));
+
+                            if (dictNano == null)
+                                continue;
+
+                            _buffEntries.Add(new BuffEntry
+                            {
+                                Character = requestor,
+                                NanoEntry = dictNano
+                            });
+                        }
+                    }
+                    break;
+                default:
+                    {
+                        Logger.Warning($"Received unknown command: {msg.Message}");
+                        break;
+                    }
+            }
+        }
+
+        private void OnUpdate(object _, double deltaTime)
+        {
+            if (_buffEntries.Count == 0)
+                return;
+
+            _graceTime -= deltaTime;
+
+            if (_graceTime < 0)
+                AttemptToBuff(_currentBuffEntry);
+
+            _waitTime -= deltaTime;
+
+            if (_waitTime < 0)
+                NextBuff();
+        }
+
+        private void OnMessageReceived(object _, Message msg)
+        {
+            if (msg.Header.PacketType != PacketType.N3Message)
+                return;
+
+            N3Message n3Msg = (N3Message)msg.Body;
+
+            if (n3Msg.N3MessageType != N3MessageType.CharacterAction)
+                return;
+
+            CharacterActionMessage charActionMessage = (CharacterActionMessage)n3Msg;
+
+            if (charActionMessage.Action == CharacterActionType.AcceptTeamRequest)
+            {
+                _isInTeam = true;
+            }
+            else if (charActionMessage.Action == CharacterActionType.FinishNanoCasting)
+            {
+                if (charActionMessage.Identity.Instance != Client.LocalDynelId)
+                    return;
+
+                _buffEntries.RemoveAt(0);
+                _currentBuffEntry = null;
+                _waitTime = 0;
+
+                if (_isInTeam)
+                {
+                    Team.LeaveTeam();
+                    _sentTeamRequest = false;
+                    _isInTeam = false;
+                }
+
+                if (charActionMessage.Parameter2 == Nanos.SloughingCombatField) 
+                    Extensions.RemoveBuff(231055);
+            }
+        }
+
+        private void NextBuff()
+        {
+            _currentBuffEntry = _buffEntries.FirstOrDefault();
+
+            switch (_currentBuffEntry.NanoEntry.Id)
+            {
+                case Nanos.SloughingCombatField:
+                    {
+                        _waitTime = 40f;
+                        break;
+                    }
+                case Nanos.RetoolNCU:
+                    {
+                        _currentBuffEntry.NanoEntry.Id = Extensions.FindBestNcuNano(_currentBuffEntry.Character.Level);
+                        _waitTime = 12f;
+                        break;
+                    }
+                default:
+                    {
+                        _waitTime = 10f;
+                        break;
+                    }
+            }
+        }
+
+        private void AttemptToBuff(BuffEntry buffEntry)
+        {
+            if (buffEntry == null)
+                return;
+
+            if (buffEntry.NanoEntry.Type == CastType.Team)
+            {
+                if (!_sentTeamRequest)
+                {
+                    Team.Invite(buffEntry.Character);
+                    _sentTeamRequest = true;  
+                }
+
+                if (!_isInTeam)
+                    return;
+            }
+
+            DynelManager.LocalPlayer.Cast(buffEntry.Character, buffEntry.NanoEntry.Id);
+            _graceTime = 0.5f;
+        }
+    }
+}
